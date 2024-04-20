7fcfdfa5e8ffc3935ee30abaef94ae7357eeadc8
Lovnish Verma <54941901+lovnishverma@users.noreply.github.com>
2024-04-20 22:25:35 +0530

    Update motion_2.0.ino
    INVERTED RELAY LOGIC NOT CORRECT

--- motion_2.0.ino
+++ motion_2.0.ino
@@ -1,6 +1,6 @@
-const int pirPin = 5;    // PIR sensor pin (GPIO 5 / D1)
-const int relayPin = 4;  // Relay module pin (GPIO 4 / D2)
-const int buzzerPin = 3; // Buzzer pin (Choose appropriate pin)
+const int pirPin = 5;     // PIR sensor pin (GPIO 5 / D1)
+const int relayPin = 4;   // Relay module pin (GPIO 4 / D2)
+const int buzzerPin = 3;  // Buzzer pin (Choose appropriate pin)
 
 unsigned long lastMotionTime = 0;
 const unsigned long motionDebounceTime = 2000; // Motion debounce time in milliseconds
@@ -13,7 +13,7 @@ void setup() {
   pinMode(relayPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 
-  digitalWrite(relayPin, LOW); // Initialize relay to off state
+  digitalWrite(relayPin, HIGH); // Initialize relay to off state
   digitalWrite(buzzerPin, LOW); // Initialize buzzer to off state
 
   Serial.begin(9600);
@@ -24,15 +24,16 @@ void loop() {
   int pirState = digitalRead(pirPin);
 
   if (pirState == HIGH) {
+    lastMotionTime = millis(); // Reset the timer whenever motion is detected
+    
     if (!lightsActivated) {
       lightsActivated = true;
       Serial.println("Motion detected! Turning on lights.");
       activateLights();
       activateBuzzer(1000); // Activate buzzer with frequency 1000Hz
     }
-    lastMotionTime = millis(); // Reset the timer whenever motion is detected
   } else {
-    if (millis() - lastMotionTime >= motionDebounceTime && lightsActivated) {
+    if (lightsActivated && (millis() - lastMotionTime >= motionDebounceTime)) {
       if (millis() - lastMotionTime >= lightsOnDuration) {
         lightsActivated = false;
         deactivateLights();
@@ -43,14 +44,14 @@ void loop() {
 }
 
 void activateLights() {
-  digitalWrite(relayPin, LOW); // Turn off the relay
+  digitalWrite(relayPin, HIGH); // Turn on the relay
   Serial.println("Lights Activated.");
 
   // No need for delay here
 }
 
 void deactivateLights() {
-  digitalWrite(relayPin, HIGH);  // Turn on the relay
+  digitalWrite(relayPin, LOW);  // Turn off the relay
   Serial.println("No motion detected. Lights Deactivated.");
 }
 
